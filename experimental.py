4ea9cdf6564ebf7b2f9a3b9afce8856c0b875fff
Meri  Hayrumyan <hayrumyanmeri@gmail.com>
2026-07-02 17:15:37 +0400

    Homework is done

--- experimental.py
+++ experimental.py
@@ -1,4 +1,5 @@
-print("Sample code", end="\n\n")
-print("continue")
-print("studying python\n\n")
-print("keep going")
+age = int(input("How old are you?\n"))
+if age > 0:
+    print(f"You are {age//10} decades and {age%10} \nyear(s) old.")
+else:
+    print("Please enter a valid age.")
